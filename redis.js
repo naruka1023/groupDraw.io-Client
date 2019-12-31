ce95e2c336438d422d724ec18b5a5319d4114f9a
Pan Muangsaen <naruka1023@yahoo.com>
2019-12-31 23:28:10 +0700

    fdsa

--- redis.js
+++ redis.js
@@ -1,5 +1,6 @@
 var redis = require('redis');
 url = process.env.REDIS_SERVER
+console.log(url)
 module.exports = function(){
   return redis.createClient(url.toString(), { return_buffers: true });
 };
