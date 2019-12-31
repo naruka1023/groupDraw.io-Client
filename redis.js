99031e4c60a93d4b0d60aa41f3de5c5345cac683
Pan Muangsaen <naruka1023@yahoo.com>
2019-12-31 22:35:18 +0700

    fdsasdfdsa

--- redis.js
+++ redis.js
@@ -1,5 +1,5 @@
 var redis = require('redis');
 url = process.env.REDIS_SERVER
 module.exports = function(){
-  return redis.createClient(url, { return_buffers: true });
+  return redis.createClient(url.toString(), { return_buffers: true });
 };
