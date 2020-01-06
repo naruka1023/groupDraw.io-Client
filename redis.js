97eae6f187a3a44033a6bde6b17ea6680e200913
Sirikarn Chareonnivassakul <naruka1023@yahoo.com>
2020-01-06 11:25:14 +0700

    fdsasd

--- redis.js
+++ redis.js
@@ -1,7 +1,6 @@
 var redis = require('redis');
 url = process.env.REDISCLOUD_URL
 module.exports = function(){
-  client = redis.createClient(redisURL.port, redisURL.hostname, { return_buffers: true });
-  client.auth(redisURL.auth.split(":")[1]);
+  client = redis.createClient(url, { return_buffers: true });
   return client
 };
